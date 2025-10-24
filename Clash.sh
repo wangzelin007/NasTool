48e3e1525a6279cfdce6fe78c5b6eebf1535971b
ZelinWang <zelinwang@microsoft.com>
2025-10-24 19:47:19 +0800

    minor fix

--- Clash.sh
+++ Clash.sh
@@ -1,4 +1,3 @@
-docker pull docker.xuanyuan.run/dreamacro/clash:latest
 docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/dreamacro/clash:latest
 docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/dreamacro/clash:latest dreamacro/clash:latest
 docker run -d \
